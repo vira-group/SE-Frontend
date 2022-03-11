d03811544546663082f6eb97e2b095fffbc153d9
HooraFarhangi <hoora9977@gmail.com>
2022-03-12 01:28:25 +0330

    Added route for homepage and homepage base file

--- hotel-center/src/App.js
+++ hotel-center/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
+import "./css/Homepage.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
