a4e2d79dfde979ac56954511ef0fe92415ebf4f4
HooraFarhangi <hoora9977@gmail.com>
2022-03-13 21:52:00 +0330

    Created a new design for navbar

--- hotel-center/src/components/Homepage/Homepage.js
+++ hotel-center/src/components/Homepage/Homepage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./layouts/Navbar";
+import Background from "../../statics/img/background3.png";
 
 export default function Homepage() {
   return (
     <div>
       <Navbar />
+      <div>
+        <img
+          src={Background}
+          alt="Background image"
+          style={{ width: "100%" }}
+        />
+      </div>
     </div>
   );
 }
--- hotel-center/src/components/Homepage/layouts/Navbar.js
+++ hotel-center/src/components/Homepage/layouts/Navbar.js
@@ -1,49 +1,94 @@
-import React, { useState } from "react";
-import Logo from "../../../statics/logo/logo.png";
+import React, { useState, useEffect, useCallback, Component } from "react";
+import Logo from "../../../statics/logo/logo2.png";
 import UseAnimations from "react-useanimations";
 import menu3 from "react-useanimations/lib/menu3";
 
-function Navbar(props) {
-  const [openMenu, setOpenMenu] = useState(false);
-  return (
-    <nav className="navbar navbar-expand-sm navbar-dark nav-background">
-      <div className="container">
-        <a href="#" className="navbar-brand logo">
-          <img src={Logo} alt="Hotel Center" />
-          <div className="text-theme-white">Hotel Center</div>
-        </a>
+class Navbar extends Component {
+  state = {
+    openMenu: false,
+    navbarMoved: false,
+  };
 
-        <button
-          className="navbar-toggler nav-hamburger-button"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navMenu"
-        >
-          <UseAnimations
-            reverse={openMenu}
-            onClick={() => {
-              setOpenMenu(!openMenu);
-            }}
-            size={40}
-            animation={menu3}
-            speed={3}
-            strokeColor="#f8f9fa"
-          />
-        </button>
+  componentDidMount() {
+    document.scrollingElement.scrollTop = 0;
+    document.addEventListener("scroll", this.handleScroll);
+  }
 
-        <div className="collapse navbar-collapse" id="navMenu">
-          <div className="ms-auto pt-3 pt-sm-0">
-            <button type="button" class="btn btn-outline-light me-2 nav-button">
-              Login
-            </button>
-            <button type="button" class="btn btn-outline-light nav-button">
-              Sign up
-            </button>
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleOpenMenu = () => {
+    this.state.openMenu
+      ? this.setState({ openMenu: false })
+      : this.setState({ openMenu: true });
+  };
+
+  handleScroll = () => {
+    var scrolled = document.scrollingElement.scrollTop;
+    if (scrolled >= 10) {
+      if (!this.state.navbarMoved) {
+        this.setState({
+          navbarMoved: true,
+        });
+      }
+    } else {
+      if (this.state.navbarMoved) {
+        this.setState({
+          navbarMoved: false,
+        });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <nav
+        className={
+          this.state.navbarMoved
+            ? "navbar navbar-expand-sm navbar-light sticky-top nav-scrolled w-100 nav-style"
+            : "navbar navbar-expand-sm navbar-light sticky-top nav-top w-100 nav-style"
+        }
+      >
+        <div className="container">
+          <a href="#" className="navbar-brand logo">
+            <img src={Logo} alt="Hotel Center" />
+            <span className="fw-bold">Hotel Center</span>
+          </a>
+
+          <button
+            className="navbar-toggler nav-hamburger-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navMenu"
+          >
+            <UseAnimations
+              reverse={this.state.openMenu}
+              onClick={this.handleOpenMenu}
+              size={40}
+              animation={menu3}
+              speed={3}
+              strokeColor="#cd9a2d"
+            />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navMenu">
+            <div className="ms-auto pt-3 pt-sm-0">
+              <button
+                type="button"
+                class="btn btn-outline-dark me-2 nav-button"
+              >
+                Login
+              </button>
+              <button type="button" class="btn btn-outline-dark nav-button">
+                Sign up
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
