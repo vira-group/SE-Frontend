6cbd8e78690d647777d12f2c035a80694686c14a
HooraFarhangi <hoora9977@gmail.com>
2022-03-16 00:28:27 +0330

    Added a field for number of guests

--- hotel-center/src/components/Homepage/layouts/SearchForm.js
+++ hotel-center/src/components/Homepage/layouts/SearchForm.js
@@ -1,11 +1,14 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Autocomplete from "@mui/material/Autocomplete";
+import Popover from "@mui/material/Popover";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DatePicker from "@mui/lab/DatePicker";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import { GoldenTextField } from "../../../theme/GoldenTextField";
 
 const Cities = [
@@ -18,12 +21,36 @@ function SearchForm(props) {
   const [checkinDate, setCheckinDate] = useState(null);
   const [checkoutDate, setCheckoutDate] = useState(null);
   const [destination, setDestination] = useState(null);
+  const [anchor, setAnchor] = React.useState(null);
+  const [numberOfAdults, setNumberOfAdults] = React.useState(1);
+  const [numberOfChildren, setNumberOfChildren] = React.useState(0);
 
   // useEffect(() => {
   //   console.log(destination);
   //   console.log(checkinDate);
   //   console.log(checkoutDate);
   // }, [destination, checkinDate, checkoutDate]);
+
+  const handleClick = (event) => {
+    setAnchor(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchor(null);
+  };
+
+  const handleChangeNumber = (actionType, guestType) => {
+    actionType === "dec"
+      ? guestType === "adults"
+        ? setNumberOfAdults(numberOfAdults > 1 ? numberOfAdults - 1 : 1)
+        : setNumberOfChildren(numberOfChildren > 0 ? numberOfChildren - 1 : 0)
+      : guestType === "adults"
+      ? setNumberOfAdults(numberOfAdults + 1)
+      : setNumberOfChildren(numberOfChildren + 1);
+  };
+
+  const open = Boolean(anchor);
+  const id = open ? "popover" : undefined;
 
   return (
     <div className="card search-form-card">
@@ -102,6 +129,17 @@ function SearchForm(props) {
             )}
           />
         </LocalizationProvider>
+        <GoldenTextField
+          className="ms-2"
+          aria-describedby={id}
+          variant="outlined"
+          onClick={handleClick}
+          label="Number of guests"
+          value={
+            numberOfAdults + " adults" + " - " + numberOfChildren + " children"
+          }
+          placeholder="0 adults - 0 children"
+        />
         <div>
           <button type="button" className="btn btn-primary ms-2 search-button">
             <span>
@@ -110,6 +148,69 @@ function SearchForm(props) {
           </button>
         </div>
       </div>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchor}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <div className="p-3 number-of-guests-form">
+          <div className="mb-3 d-flex align-items-center">
+            <p className="mb-0 me-auto">Adults</p>
+            <button
+              type="button"
+              className="btn btn-primary decrease-button"
+              onClick={() => handleChangeNumber("dec", "adults")}
+              disabled={numberOfAdults <= 1}
+            >
+              <span>
+                <RemoveIcon />
+              </span>
+            </button>
+            <p className="mb-0 px-3">{numberOfAdults}</p>
+            <button
+              type="button"
+              className="btn btn-primary increase-button"
+              onClick={() => handleChangeNumber("inc", "adults")}
+            >
+              <span>
+                <AddIcon />
+              </span>
+            </button>
+          </div>
+          <div className="d-flex  align-items-center">
+            <p className="mb-0 me-auto">Children</p>
+            <button
+              type="button"
+              className="btn btn-primary decrease-button"
+              onClick={() => handleChangeNumber("dec", "children")}
+              disabled={numberOfChildren <= 0}
+            >
+              <span>
+                <RemoveIcon />
+              </span>
+            </button>
+            <p className="mb-0 px-3">{numberOfChildren}</p>
+            <button
+              type="button"
+              className="btn btn-primary increase-button"
+              onClick={() => handleChangeNumber("inc", "children")}
+            >
+              <span>
+                <AddIcon />
+              </span>
+            </button>
+          </div>
+        </div>
+      </Popover>
     </div>
   );
 }
