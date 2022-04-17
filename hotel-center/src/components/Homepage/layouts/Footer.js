03e2f514e51e89649a496fb721bd85d0c479f96a
HooraFarhangi <hoora9977@gmail.com>
2022-04-17 19:55:25 +0430

    Fixed footer responsive

--- hotel-center/src/components/Homepage/layouts/Footer.js
+++ hotel-center/src/components/Homepage/layouts/Footer.js
@@ -5,13 +5,13 @@ function Footer() {
     <div className="footer-bg-color">
       <div className="container py-5">
         <div className="row text-white">
-          <div className="col-md-4">
+          <div className="col-md-4 mb-4 mb-md-0">
             <p className="fw-bold fs-5">Support</p>
             <p className="fw-light">Help center</p>
             <p className="fw-light">Safety information</p>
             <p className="mb-0 fw-light">How to register a hotel</p>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-4 mb-4 mb-md-0">
             <p className="fw-bold fs-5">Booking tutorial</p>
             <p className="fw-light">Hotel reservation guidance</p>
             <p className="fw-light">Payment methods</p>
