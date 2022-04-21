a570b1c4ae9626014f54375174a0a2ed71c5975e
HooraFarhangi <hoora9977@gmail.com>
2022-04-21 19:04:17 +0430

    Added some tests

--- hotel-center/src/App.test.js
+++ hotel-center/src/App.test.js
@@ -1,10 +1,97 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// hello.test.js, again
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />); //render is from @testing-library/react
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument(); //expect assertion is from Jest
+// import React from "react";
+// import { render, unmountComponentAtNode } from "react-dom";
+// import { act } from "react-dom/test-utils";
+// import pretty from "pretty";
+
+// import Hello from "./hello";
+
+// let container = null;
+// beforeEach(() => {
+//   // setup a DOM element as a render target
+//   container = document.createElement("div");
+//   document.body.appendChild(container);
+// });
+
+// afterEach(() => {
+//   // cleanup on exiting
+//   unmountComponentAtNode(container);
+//   container.remove();
+//   container = null;
+// });
+
+// it("should render a greeting", () => {
+//   act(() => {
+//     render(<Hello />, container);
+//   });
+
+//   expect(
+// pretty(container.innerHTML)).
+// toMatchInlineSnapshot(`"<span>Hey, stranger</span>"`); /* ... gets filled automatically by jest ... */
+
+//   act(() => {
+//     render(<Hello name="Jenny" />, container);
+//   });
+
+//   expect(
+// pretty(container.innerHTML)).
+// toMatchInlineSnapshot(`"<h1>Hello, Jenny!</h1>"`); /* ... gets filled automatically by jest ... */
+
+//   act(() => {
+//     render(<Hello name="Margaret" />, container);
+//   });
+
+//   expect(
+// pretty(container.innerHTML)).
+// toMatchInlineSnapshot(`"<h1>Hello, Margaret!</h1>"`); /* ... gets filled automatically by jest ... */
+// });
+
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import Header from "./components/Homepage/layouts/Header";
+import Footer from "./components/Homepage/layouts/Footer";
+import { render, screen } from "@testing-library/react";
+import { faParagraph } from "@fortawesome/free-solid-svg-icons";
+import { toBeChecked } from "@testing-library/jest-dom/dist/matchers";
+
+// test("renders the correct content", () => {
+//   //Render a React component to the DOM
+//   const root = document.createElement("div");
+//   ReactDom.render(<Footer />, root);
+
+//   //Use DOM APIs (querySelector) to make assertions
+//   expect(root.querySelector("p").textContent).toBe("Support");
+// });
+
+it("should render the correct content", async () => {
+  render(<Footer />);
+  const FooterElement = screen.getByText(/Support/i);
+  expect(FooterElement).toBeInTheDocument();
 });
+
+it("should render the correct content", async () => {
+  render(<Footer />);
+  const FooterElement = screen.getByTitle("header2");
+  expect(FooterElement).toBeInTheDocument();
+});
+
+it("should render the correct content", async () => {
+  render(<Footer />);
+  const FooterElement = await screen.findByText(/Magazine/i);
+  expect(FooterElement).toBeInTheDocument();
+});
+
+it("should render the correct content", async () => {
+  render(<Footer />);
+  const FooterElements = screen.queryAllByRole("button");
+  expect(FooterElements.length).toBe(0);
+});
+
+it("should render the correct content", async () => {
+  render(<Footer />);
+  const FooterElements = screen.queryByText(/cars/i)
+  expect(FooterElements).not.toBeInTheDocument();
+});
+
 
--- hotel-center/src/components/Homepage/layouts/Footer.js
+++ hotel-center/src/components/Homepage/layouts/Footer.js
@@ -6,19 +6,25 @@ function Footer() {
       <div className="container py-5">
         <div className="row text-white">
           <div className="col-md-4 mb-4 mb-md-0">
-            <p className="fw-bold fs-5">Support</p>
+            <h1 className="fw-bold fs-5" title="header1">
+              Support
+            </h1>
             <p className="fw-light">Help center</p>
             <p className="fw-light">Safety information</p>
             <p className="mb-0 fw-light">How to register a hotel</p>
           </div>
           <div className="col-md-4 mb-4 mb-md-0">
-            <p className="fw-bold fs-5">Booking tutorial</p>
+            <p className="fw-bold fs-5" title="header2">
+              Booking tutorial
+            </p>
             <p className="fw-light">Hotel reservation guidance</p>
             <p className="fw-light">Payment methods</p>
             <p className="mb-0 fw-light">Cancellation options</p>
           </div>
           <div className="col-md-4">
-            <p className="fw-bold fs-5">About Us</p>
+            <p className="fw-bold fs-5" title="header3">
+              About Us
+            </p>
             <p className="fw-light">Rules</p>
             <p className="fw-light">Magazine</p>
             <p className="mb-0 fw-light">Contact us</p>
