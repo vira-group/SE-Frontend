e891462ee5e3acd4e5fc9e8053b97a49540305c2
Fatima <56315235+fatememirjalili2000@users.noreply.github.com>
2022-04-27 17:54:42 +0430

    Create reservation.jsx

---
+++ hotel-center/src/components/Reservation/reservation.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class reservation extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( <div>
+                <div className="containter">
+                    <
+                </div>
+
+        </div> );
+    }
+}
+ <div>
+
+
+ </div>
+e               <div className="containter">
+<</div>xport default reservation;
