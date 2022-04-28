7efde76b6a0426bdc1cc7bfa546e87dfd5b47667
Fatima <56315235+fatememirjalili2000@users.noreply.github.com>
2022-04-28 19:58:17 +0430

    Update reservation.jsx

--- hotel-center/src/components/Reservation/reservation.jsx
+++ hotel-center/src/components/Reservation/reservation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css';
+// import 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css';
 import '../../css/Hotelpage.css';
 class reservation extends Component {
 	constructor(props) {
@@ -13,38 +13,27 @@ class reservation extends Component {
 					<div className="row justify-content-center ">
 						<div className="col-12 col-md-4">
 							<div className="card-containter">
-								{/* <div className="card"> */}
 									<div className="card-body">
 										<div class="shadow p-3 mb-5 bg-body rounded">
-										
+										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
+  <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
+</svg>
 
-										<ion-icon name="calendar-outline"></ion-icon>
+
 										<div className="">
 												<hr className="m-3 hr-text" />
-											</div><ion-icon name="calendar"></ion-icon><ion-icon name="wallet-outline"></ion-icon>ff
-										<ion-icon name="calendar-outline"></ion-icon>
-										<ion-icon name="calendar-outline"></ion-icon><i class="bi bi-calendar-date"></i>
-											dd
+											</div>
 											
-											<div className="div">
+											<i class="bi bi-calendar-date-fill"></i>
 
-											<span class='bi bi-calendar-date' style='color:#cd9a2d'></span>
-
-											</div>
-											<div className="">
-												<hr className="m-3 hr-text" />
-											</div>
-											dd
-											<div className="">
-												<hr className="m-3 hr-text" />
-											</div>
-dd
+											 </div>
+											
 
 
 
 
-{/* 										
-										<h5 className="card-title">Card title</h5>
+										
+										{/* <h5 className="card-title">Card title</h5>
 											<p className="card-text">
 										
 											</p>
@@ -66,9 +55,8 @@ dd
 
 
 											
-										</div>
-									</div>
-								{/* </div> */}
+								
+								</div> 
 							</div>
 						</div>
 
