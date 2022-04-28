4784f0c8fe3ef28bbb4bfb2bd8e9e2a3b59d7321
Fatima <56315235+fatememirjalili2000@users.noreply.github.com>
2022-04-28 16:59:10 +0430

    Update reservation.jsx

--- hotel-center/src/components/Reservation/reservation.jsx
+++ hotel-center/src/components/Reservation/reservation.jsx
@@ -12,27 +12,33 @@ class reservation extends Component {
 				<div className="containter">
 					<div className="row justify-content-center ">
 						<div className="col-12 col-md-4">
-							<div className="card">
-								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
-									<p className="card-text">
-										Some quick example text to build on the card title and make up the bulk of the
-										card's content.
-									</p>
-								
-									<button className="btn btn-primary" type="button" 
-									// disabled
-									>
-										{/* <span
+							<div className="card-containter">
+								<div className="card">
+									<div className="card-body">
+										<div class="shadow p-3 mb-5 bg-body rounded">
+											Regular shadowx <h5 className="card-title">Card title</h5>
+											<p className="card-text">
+												Some quick example text to build on the card title and make up the bulk
+												of the card's content.
+											</p>
+											<button
+												className="btn btn-primary"
+												type="button"
+												// disabled
+											>
+												{/* <span
 											className="spinner-border spinner-border-sm"
 											role="status"
 											aria-hidden="true"
 										/> */}
-										Loading
-									</button>
-</div>
+												Loading
+											</button>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
+
 						<div className="col-12 col-md-8">
 							<div className="card">
 								<div className="card-body">
@@ -41,9 +47,6 @@ class reservation extends Component {
 										Some quick example text to build on the card title and make up the bulk of the
 										card's content.
 									</p>
-									
-
-
 								</div>
 							</div>
 						</div>
