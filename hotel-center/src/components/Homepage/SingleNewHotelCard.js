98ebad4c04765cec8ef69849c7ed962d2992d4af
YasamanFarhangi <yf7901@gmail.com>
2022-07-08 02:04:59 +0430

    connecting best hotels and new hotels

--- hotel-center/src/components/Homepage/SingleNewHotelCard.js
+++ hotel-center/src/components/Homepage/SingleNewHotelCard.js
@@ -1,15 +1,60 @@
+import React from "react";
 import { useState } from "react";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import StarIcon from "@mui/icons-material/Star";
+import axios from "axios";
+import { cookies, makeURL, set_cookie } from "../../Utils/common";
+import references from "../../assets/References.json";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 export default function SingleNewHotelCard(props) {
   const [isFavorite, setIsFavorite] = useState(props.isFavorite);
+  const [message, setMessage] = useState("");
+  const [open, setOpen] = useState(false);
 
   // handle request to backend when click on heart button
   const handleClick = () => {
     setIsFavorite(!isFavorite);
     // request to backend
+    axios
+      .post(
+        makeURL(references.url_addfavoritehotel),
+        { hotel_id: props.id },
+        {
+          headers: {
+            Authorization: cookies.get("Authorization"),
+          },
+        }
+      )
+      .then((response) => {
+        console.log("adding favorite hotel: ", response.data);
+        setOpen(true);
+        setMessage(
+          isFavorite === false
+            ? "Hotel was added to your favorites successfully!"
+            : "Hotel was removed from your favorites successfully!"
+        );
+        setTimeout(() => window.location.reload(true), 1000);
+      })
+      .catch((error) => {
+        console.log("failed to add favorite hotel: ", error);
+        setOpen(true);
+        setMessage("An error occured.Please try again.");
+      });
+  };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
@@ -40,6 +85,24 @@ export default function SingleNewHotelCard(props) {
               <FavoriteBorderIcon />
             )}
           </button>
+          <Snackbar
+            open={open}
+            autoHideDuration={4000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          >
+            <Alert
+              onClose={handleClose}
+              severity={
+                message === "An error occured.Please try again."
+                  ? "error"
+                  : "success"
+              }
+              sx={{ width: "100%" }}
+            >
+              {message}
+            </Alert>
+          </Snackbar>
         </div>
       </div>
     </div>
--- hotel-center/src/components/Homepage/TopHotels.js
+++ hotel-center/src/components/Homepage/TopHotels.js
@@ -1,20 +1,47 @@
 import React from "react";
 import SingleTopHotelCard from "./SingleTopHotelCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { cookies, makeURL, set_cookie } from "../../Utils/common";
+import references from "../../assets/References.json";
+
+
 
 export default function TopHotels(props) {
+  const[besthotels, setBesthotels] = useState(null);
+  
+
+  
+
+  useEffect(() => {
+    axios.get(makeURL(references.url_besthotels),{
+      headers: {
+        Authorization: cookies.get("Authorization"),
+      },
+    })
+    .then((response) => {
+      console.log("this is the response for best hotels: ",response.data);
+      setBesthotels(response.data);
+    })
+    .catch((error) => {
+      console.log("this is the error for best hotels request: ", error)
+    })
+  },[])
+
   return (
     <div className="container top-hotels mt-5">
       <h3 className="mb-4 fw-bold">Top Hotels</h3>
       <div class="row row-cols-1 row-cols-md-4 g-4">
-        {props.hotels
-          ? props.hotels.map((hotel) => (
+        {besthotels
+          ? besthotels.map((hotel) => (
               <SingleTopHotelCard
-                image={hotel.image}
+                image={hotel.header}
                 name={hotel.name}
                 city={hotel.city}
                 country={hotel.country}
-                score={hotel.score}
-                isFavorite={hotel.isFavorite}
+                score={hotel.rate}
+                isFavorite={hotel.is_favorite}
+                id={hotel.id}
               />
             ))
           : null}
