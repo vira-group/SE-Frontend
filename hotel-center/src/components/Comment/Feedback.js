e14e2f444e0209ba3142b67c89957dfc0881eb63
Fatima <56315235+fatememirjalili2000@users.noreply.github.com>
2022-07-08 11:14:30 +0430

    Update Feedback.js

--- hotel-center/src/components/Comment/Feedback.js
+++ hotel-center/src/components/Comment/Feedback.js
@@ -4,17 +4,16 @@ import Comment from './Components/Comment';
 import AddComment from './Components/AddComment';
 import axios from 'axios';
 import { cookies, makeURL, set_cookie } from '../../Utils/common';
-import references from "../../assets/References.json";
+import references from '../../assets/References.json';
 
 const Feedback = (props) => {
 	const [ comments, updateComments ] = useState([]);
-	const [ comments1, updateComments1 ] = useState([]);
-	const [ comments2, updateComments2 ] = useState([]);
 	const [ deleteModalState, setDeleteModalState ] = useState(false);
 
 	const [ c, setc ] = useState(null);
-	const [ first, setFirst ] = useState("");
-	const [ last, setlast ] = useState("");
+	const [ c1, setc1 ] = useState(null);
+	const [ first, setFirst ] = useState('');
+	const [ last, setlast ] = useState('');
 	const [ avatar, setAvatar ] = useState(null);
 	const [ hotelid, sethotelid ] = useState(null);
 	const [ comId, setComid ] = useState(null);
@@ -25,25 +24,23 @@ const Feedback = (props) => {
 	// 	updateComments(data.comments);
 	// };
 
-	
 	useEffect(() => {
 		axios
-		  .get(makeURL(references.url_edit_profile), {
-			headers: {
-			  Authorization: cookies.get("Authorization"),
-			},
-		  })
-		  .then((res) => {
-			console.log('response of profile: ',res.data);
-			setAvatar(res.data.avatar);
-			setFirst(res.data.firstName);
-			setlast(res.data.lastName);
-		
-		  });
-	  }, []);
+			.get(makeURL(references.url_edit_profile), {
+				headers: {
+					Authorization: cookies.get('Authorization')
+				}
+			})
+			.then((res) => {
+				console.log('response of profile: ', res.data);
+				setAvatar(res.data.avatar);
+				setFirst(res.data.firstName);
+				setlast(res.data.lastName);
+			});
+	}, []);
 
-	  console.log(first);
-	  console.log(last);
+	console.log(first);
+	console.log(last);
 
 	useEffect(() => {
 		const hotelid = props.id;
@@ -54,9 +51,8 @@ const Feedback = (props) => {
 			.then((response) => {
 				console.log('hotel comments', response.data);
 				setc(response.data);
-				// console.log(comments, 'comments get all hotel');
-				// updateComments1(response.data.slice(0, 10).text);
-				// updateComments2(response.data.slice(11, 49).text);
+
+				setc1(response.data.slice(0, 10));
 			})
 			.catch((error) => {
 				console.log(error, 'comment error');
@@ -144,10 +140,6 @@ const Feedback = (props) => {
 		// 		console.log(error, 'comment delete error');
 		// 	});
 	}, []);
-
-	// console.log(c, 'setc');
-	// console.log(c1, 'setc111111');
-
 	useEffect(() => {
 		// localStorage.getItem('comments') !== null
 		// 	? updateComments(JSON.parse(localStorage.getItem('comments')))
@@ -156,35 +148,13 @@ const Feedback = (props) => {
 
 	useEffect(
 		() => {
-			localStorage.setItem('comments', JSON.stringify(comments));
+			// localStorage.setItem('comments', JSON.stringify(comments));
 			deleteModalState
 				? document.body.classList.add('overflow--hidden')
 				: document.body.classList.remove('overflow--hidden');
 		},
-		[ comments, deleteModalState ]
+		[ deleteModalState ]
 	);
-
-	// update score
-	let updateScore = (score, id, type) => {
-		let updatedComments = [ ...comments ];
-
-		if (type === 'comment') {
-			updatedComments.forEach((data) => {
-				if (data.id === id) {
-					data.score = score;
-				}
-			});
-		} else if (type === 'reply') {
-			updatedComments.forEach((comment) => {
-				comment.replies.forEach((data) => {
-					if (data.id === id) {
-						data.score = score;
-					}
-				});
-			});
-		}
-		updateComments(updatedComments);
-	};
 
 	// add comments
 	let addComments = (newComment) => {
@@ -213,14 +183,6 @@ const Feedback = (props) => {
 					data.content = content;
 				}
 			});
-		} else if (type === 'reply') {
-			updatedComments.forEach((comment) => {
-				comment.replies.forEach((data) => {
-					if (data.id === id) {
-						data.content = content;
-					}
-				});
-			});
 		}
 
 		updateComments(updatedComments);
@@ -234,96 +196,37 @@ const Feedback = (props) => {
 		if (type === 'comment') {
 			updatedComments = updatedComments.filter((data) => data.id !== id);
 		}
-		//  else if (type === 'reply') {
-		// 	comments.forEach((comment) => {
-		// 		if (comment.id === parentComment) {
-		// 			updatedReplies = comment.replies.filter((data) => data.id !== id);
-		// 			comment.replies = updatedReplies;
-		// 		}
-		// 	});
-		// }
-
 		updateComments(updatedComments);
 	};
 
-	console.log(comments, 'commmmmmmmmmmmmmmm');
-	
-										// console.log((c[0].created_at.split("T"))[0]   , 'commmmmmmmmmmmmmmm');
-
 	return (
 		<main className="Feedback">
-			{' '}
 			<div className="col-lg-8">
 				<div className="row">
 					<div className="col">
 						<div className="mb-3 row">
 							<div className="col">
-								{/* <div>{c}</div> */}
-								{/* <div>{c1}</div> */}
-
-								{/* {c? (c.map((comment) => (
-									<Comment
-										key={comment.id}
-										commentData={comment}
-										// updateScore={updateScore}
-										// updateReplies={updateReplies}
-										// editComment={editComment}
-										// commentDelete={commentDelete}
-										// setDeleteModalState={setDeleteModalState}
-									/>
-								))) : "hello mistake"} */}
-
-
-								{Array.isArray(c) ? (c).map(e => 	<Comment
-										key={e.id}
-										commentData={e.text}
-										rate={e.rate}
-										time = {(((e.created_at.split("T"))[1]).split("."))[0]}
-										first = {first}
-										last={last}
-										avatar={avatar}
-										hotelid={hotelid}
-										comId={comId}
-										// updateScore={updateScore}
-										// updateReplies={updateReplies}
-										// editComment={editComment}
-										// commentDelete={commentDelete}
-										// setDeleteModalState={setDeleteModalState}
-									/>
+								{Array.isArray(c1) ? (
+									c1.map((e) => (
+										<Comment
+											key={e.id}
+											commentData={e.text}
+											rate={e.rate}
+											time={e.created_at.split('T')[1].split('.')[0]}
+											first={first}
+											last={last}
+											avatar={avatar}
+											hotelid={hotelid}
+											comId={comId}
+											// updateScore={updateScore}
+											// updateReplies={updateReplies}
+											// editComment={editComment}
+											// commentDelete={commentDelete}
+											// setDeleteModalState={setDeleteModalState}
+										/>
+									))
 								) : null};
-
-
-
-								{/* {((c) => (
-									<Comment
-										key={1}
-										commentData={c}
-										updateScore={updateScore}
-										updateReplies={updateReplies}
-										editComment={editComment}
-										commentDelete={commentDelete}
-										setDeleteModalState={setDeleteModalState}
-									/>
-								))} */}
 								<AddComment buttonValue={'send'} addComments={addComments} />
-								{/* {f1 ? (
-											f1.map((f) => (
-												<div className="mb-3 row">
-													<div className="col">{f.name}</div>
-													<div className="col">{Icons[f.name]}</div>
-												</div>
-											))
-										) : null} */}
-
-								{/* { c? (  c.map((a) => <div>{a.text}</div>))  : "m" } */}
-								{/* {c ? (
-											c.map((f) => (
-												<div className="mb-3 row">
-													<div className="col">{f.text}</div>
-													<div className="col">{f.rate}</div>
-												</div>
-											))
-										) : null} */}
 							</div>
 						</div>
 					</div>
@@ -358,35 +261,26 @@ const Feedback = (props) => {
 							</div>
 							<div className="modal-body">
 								<div className="container">
-									{/* {(comment) => ( */}
-									{/* <div> */}
-									{/* <Comment
-											hotelid={props.id}
-											commentid = {comment.id}
-												
-												
-												key={comment.id}
-												commentData={comment.text}
-												updateScore={updateScore}
-												updateReplies={updateReplies}
-												editComment={editComment}
-												commentDelete={commentDelete}
-												setDeleteModalState={setDeleteModalState}
-											/> */}
-
-									{/* <hr className="mb-0 mt-0 hr-text" /> */}
-									{/* </div> */}
-									{/* )} */}
-									{/* {c ? (
-										c.map((f) => (
-											<div className="mb-3 row">
-												<div className="col">{f.text}</div>
-												<div className="col" style={{ textAlign: 'right' }}>
-													{f.rate}
-												</div>
-											</div>
-										))
-									) : null} */}
+								{Array.isArray(c) ? (
+									c.map((e) => (
+										<Comment
+											key={e.id}
+											commentData={e.text}
+											rate={e.rate}
+											time={e.created_at.split('T')[1].split('.')[0]}
+											first={first}
+											last={last}
+											avatar={avatar}
+											hotelid={hotelid}
+											comId={comId}
+											// updateScore={updateScore}
+											// updateReplies={updateReplies}
+											// editComment={editComment}
+											// commentDelete={commentDelete}
+											// setDeleteModalState={setDeleteModalState}
+										/>
+									))
+								) : null};
 								</div>
 							</div>
 						</div>
