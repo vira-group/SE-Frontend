ec0a49ef8cfe5588dcb674c28a5cde0b3c19ea13
Jack Krono <mousavi20k@gmail.com>
2023-04-22 11:44:53 +0330

    fix: Next.js image bugs
    This is a temporary fix. Need a proper slider.

--- src/components/Homepage/layouts/Header.js
+++ src/components/Homepage/layouts/Header.js
@@ -1,20 +1,50 @@
+// import React from "react";
+// import SlideOne from "../../../../public/img/slide01.jpg";
+// import SlideTwo from "../../../../public/img/slide02.jpg";
+// import SlideThree from "../../../../public/img/slide03.jpg";
+// import SearchForm from "./SearchForm";
+
+// function Header(props) {
+//   return (
+//     <div className="header">
+//       <div
+//         id="carousel"
+//         className="carousel slide w-100 "
+//         data-bs-ride="carousel"
+//       >
+//         <div className="carousel-inner">
+//           <div className="carousel-item active">
+//             <img src={SlideOne} className="d-block w-100" alt="Slide 1" />
+//           </div>
+//           <div className="carousel-item">
+//             <img src={SlideTwo} className="d-block w-100" alt="Slide 2" />
+//           </div>
+//           <div className="carousel-item">
+//             <img src={SlideThree} className="d-block w-100" alt="Slide 3" />
+//           </div>
+//         </div>
+//       </div>
+//       <div className="d-flex px-2 px- justify-content-center">
+//         <SearchForm setHotels={props.setHotels} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
 import React from "react";
 import SearchForm from "./SearchForm";
 import Image from "next/image";
 
 function Header(props) {
   return (
-    <div className="header">
+    <div className="header justify-content-center">
       <div
         id="carousel"
-        className="carousel slide w-100 "
-        data-bs-ride="carousel"
         // className="carousel slide w-100"
         // data-bs-ride="carousel"
       >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={SlideOne} className="d-block w-100" alt="Slide 1" />
         {/* <div className="carousel-indicators">
           <button
             type="button"
@@ -54,7 +84,6 @@ function Header(props) {
             </div> */}
           </div>
           <div className="carousel-item">
-            <img src={SlideTwo} className="d-block w-100" alt="Slide 2" />
             {/* <Image src={SlideTwo} className="d-block w-100" alt="Slide 2" /> */}
             <Image quality={100} fill src="/img/slide02.jpg" alt="Slide 2" />
             {/* <div className="carousel-caption d-none d-md-block">
@@ -76,7 +105,7 @@ function Header(props) {
           </div>
         </div>
       </div>
-      <div className="d-flex px-2 px- justify-content-center">
+      <div className="px-2 px-sm-0 d-flex justify-content-center">
         <SearchForm setHotels={props.setHotels} />
       </div>
     </div>
