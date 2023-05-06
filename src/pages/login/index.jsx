866fc722cad2f9135beaca71da98af8b748d36f9
Diyar Hamedi <diyarhamedi81@gmail.com>
2023-05-06 09:23:30 +0330

    fix: apply new style to login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,6 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import references from "../../assets/References.json";
@@ -19,7 +18,6 @@ import { useFormik } from "formik";
 import { useState } from "react";
 import Image from "next/image";
 
-const theme = createTheme();
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
@@ -63,112 +61,107 @@ export default function Login() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Image src="/img/icon.png" width={40} height={40} />
+
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+
+        <Image src="/img/s4.png" width={300} height={300} />
+
         <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+          component="form"
+          noValidate
+          onSubmit={formik.handleSubmit}
+          sx={{ mt: 1 }}
         >
-          <Image src="/img/icon.png" width={40} height={40} />
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+            </Grid>
 
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+            </Grid>
 
-          <Image src="/img/s4.png" width={300} height={300} />
-
-          <Box
-            component="form"
-            noValidate
-            onSubmit={formik.handleSubmit}
-            sx={{ mt: 1 }}
-          >
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                />
-              </Grid>
-
-              {/* <div>
+            {/* <div>
                 <h5 styles={{ color: "red", fontSize: "14.5px" }}>
                   {formik.errors.email}
                 </h5>
               </div> */}
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              onClick={formik.handleSubmit}
-              style={{
-                mt: 2,
-                mb: 2,
-                backgroundColor: "black",
-                color: "white",
-                marginTop: "20px",
-                height: "43px",
-                marginBottom: "10px",
-              }}
-            >
-              Login
-            </Button>
-
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/sign-up" className="lnk_Login">
-                  Don't have an account yet? Sign Up
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Snackbar
-          open={popupIsOpen}
-          autoHideDuration={2000}
-          onClose={() => setPopupIsOpen(false)}
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        >
-          <Alert
-            onClose={() => setPopupIsOpen(false)}
-            severity={
-              popupMessage ===
-              ("wrong email or password" || "Already logged in")
-                ? "error"
-                : "success"
-            }
-            sx={{ width: "100%" }}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={formik.handleSubmit}
+            style={{
+              mt: 2,
+              mb: 2,
+              marginTop: "20px",
+              height: "43px",
+              marginBottom: "10px",
+            }}
           >
-            {popupMessage}
-          </Alert>
-        </Snackbar>{" "}
-      </Container>
-    </ThemeProvider>
+            Login
+          </Button>
+
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/sign-up" className="lnk_Login">
+                Don't have an account yet? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Snackbar
+        open={popupIsOpen}
+        autoHideDuration={2000}
+        onClose={() => setPopupIsOpen(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert
+          onClose={() => setPopupIsOpen(false)}
+          severity={
+            popupMessage === ("wrong email or password" || "Already logged in")
+              ? "error"
+              : "success"
+          }
+          sx={{ width: "100%" }}
+        >
+          {popupMessage}
+        </Alert>
+      </Snackbar>{" "}
+    </Container>
   );
 }
