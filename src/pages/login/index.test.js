a8413291570b3572e081dc2db25b5753cc7519bb
Diyar Hamedi <diyarhamedi81@gmail.com>
2023-05-06 14:29:51 +0330

    test: add basic tests for alert data rendering

--- src/pages/login/index.test.js
+++ src/pages/login/index.test.js
@@ -1,4 +1,5 @@
 import Login from ".";
+import { Alert } from ".";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
@@ -37,5 +38,26 @@ describe("Login", () => {
 
     expect(emailInput).toHaveValue(email);
     expect(passwordInput).toHaveValue(password);
+  });
+});
+
+describe("Alert", () => {
+  it("renders alert text", () => {
+    const alertMessage = "Alert Message";
+    const alert = render(<Alert>{alertMessage}</Alert>);
+    expect(alert.getByText(alertMessage)).toBeTruthy();
+  });
+
+  it("shows success message properly", () => {
+    const alertMessage = "Success Message";
+    const alert = render(<Alert severity="success">{alertMessage}</Alert>);
+    console.log(alert.getByRole("alert"));
+    expect(alert.getByRole("alert")).toHaveClass("MuiAlert-filledSuccess");
+  });
+
+  it("shows error message properly", () => {
+    const alertMessage = "Error Message";
+    const alert = render(<Alert severity="error">{alertMessage}</Alert>);
+    expect(alert.getByRole("alert")).toHaveClass("MuiAlert-filledError");
   });
 });
