eefcbe929532b191e4bb48baffb368e98edbecce
Diyar Hamedi <diyarhamedi81@gmail.com>
2023-05-06 09:10:08 +0330

    fix: apply new style to signup page

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -4,8 +4,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import "./style.css";
 import SignupForm from "../../components/signup/SignupForm";
 import { useState } from "react";
 import SignupRoleSelection from "../../components/signup/SignupRoleSelection";
@@ -38,47 +36,43 @@ export default function Signup() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   }
 
-  const theme = createTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="md">
-        <CssBaseline />
-        <Box
-          spacing={8}
-          sx={{
-            mt: 4,
-            gap: 8,
-            display: "flex",
-            alignItems: "center",
-            height: "60vh",
-          }}
-        >
-          <Stack alignItems="center">
-            <Avatar sx={{ m: 0.5, backgroundColor: "black", color: "white" }} />
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Image src="/img/s3.png" width={300} height={300} />
-          </Stack>
-          <Box flexGrow={1} sx={{ height: "80%" }}>
-            {activeStep === 0 && (
-              <SignupRoleSelection
-                nextStep={handleNext}
-                role={signupState.role}
-                dispatch={dispatch}
-              />
-            )}
-            {activeStep === 1 && (
-              <SignupForm
-                prevStep={handleBack}
-                role={signupState.role}
-                dispatch={dispatch}
-              />
-            )}
-          </Box>
+    <Container component="main" maxWidth="md">
+      <CssBaseline />
+      <Box
+        spacing={8}
+        sx={{
+          mt: 4,
+          gap: 8,
+          display: "flex",
+          alignItems: "center",
+          height: "60vh",
+        }}
+      >
+        <Stack alignItems="center">
+          <Avatar sx={{ m: 0.5, backgroundColor: "black", color: "white" }} />
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Image src="/img/s3.png" width={300} height={300} />
+        </Stack>
+        <Box flexGrow={1} sx={{ height: "80%" }}>
+          {activeStep === 0 && (
+            <SignupRoleSelection
+              nextStep={handleNext}
+              role={signupState.role}
+              dispatch={dispatch}
+            />
+          )}
+          {activeStep === 1 && (
+            <SignupForm
+              prevStep={handleBack}
+              role={signupState.role}
+              dispatch={dispatch}
+            />
+          )}
         </Box>
-      </Container>
-    </ThemeProvider>
+      </Box>
+    </Container>
   );
 }
